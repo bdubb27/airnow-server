1159b0282eebe0d7a60b052a8ab974c89ca1eefd
blake <bdubb27@gmail.com>
2022-07-07 02:48:45 -0700

    added endpoints for getting the latest observation file... and adding the latest file to the db...

--- server.js
+++ server.js
@@ -3,15 +3,38 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const { getObservations } = require('./lib/sql-service.js')
+const { AirNowFile } = require('./lib/file-getter-service.js')
 
 app.use(cors())
 
 app.get('/api/observations', async (req, res) => {
-  console.log('/api/observations was called...');
   try {
     let data = await getObservations(req.query)
     res.setHeader('Content-Type', 'application/json')
     res.send(data)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+app.get('/api/observations/get', async (req, res) => {
+  let airNowFile = new AirNowFile('HourlyAQObs', new Date().getUTCMinutes() < 35 ? -2 : -1)
+  try {
+    let data = await airNowFile.getFile()
+    res.setHeader('Content-Type', 'application/json')
+    res.send(airNowFile.message)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+app.get('/api/observations/add', async (req, res) => {
+  let airNowFile = new AirNowFile('HourlyAQObs', new Date().getUTCMinutes() < 35 ? -2 : -1)
+  try {
+    let data = await airNowFile.parseFile()
+    let dres = await airNowFile.insert(data)
+    res.setHeader('Content-Type', 'application/json')
+    res.send(dres)
   } catch (error) {
     console.log(error)
   }
