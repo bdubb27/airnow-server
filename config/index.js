8bd8f652736405c74651f6deaa7e6f6d992f5de9
blake <bdubb27@gmail.com>
2022-07-11 10:51:20 -0700

    changed the name of the env variable for mysql db... now MYSQL_DATABASE

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@ const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DB,
+  database: process.env.MYSQL_DATABASE,
 })
 
 module.exports = { mysql, pool }
