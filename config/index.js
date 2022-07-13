81e0f1ddf8df690dab2a83ec57bf205a9abe9b9a
blake <bdubb27@gmail.com>
2022-07-13 16:42:30 -0700

    removed the secrets.MYSQL_DATABASE option, as db will be treated as config not secret and set via env

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: process.env.MYSQL_HOST || '127.0.0.1',
   user: secrets.MYSQL_USER || process.env.MYSQL_USER || 'root',
   password: secrets.MYSQL_PASSWORD || process.env.MYSQL_PASSWORD || 'password',
-  database: secrets.MYSQL_DATABASE || process.env.MYSQL_DATABASE || 'aqi'
+  database: process.env.MYSQL_DATABASE || 'aqi'
 })
 
 module.exports = { mysql, pool }
